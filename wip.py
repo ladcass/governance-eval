f06d4150882c6f7c136a05d62e0eb5c435e736dd
Lucas Amar <lucasamar5@gmail.com>
2024-02-24 20:23:59 +0000

    Delete wip.py

--- wip.py
+++
@@ -1,9 +0,0 @@
-import wrds
-import numpy as np
-import pandas as pd
-from math import log
-from datetime import datetime
-from thefuzz import process
-
-# Credentials to log into WRDS
-# db = wrds.Connection(wrds_username='s2029442')

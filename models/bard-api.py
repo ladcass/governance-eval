e5f668f96865c4a104b3f336c5fb35ed44e66e2e
Lucas Amar <lucasamar5@gmail.com>
2024-02-24 20:31:43 +0000

    get files in

---
+++ models/bard-api.py
@@ -0,0 +1,13 @@
+import pathlib
+import textwrap
+
+import google.generativeai as genai
+from IPython.display import display
+from IPython.display import Markdown
+
+
+genai.configure(api_key='AIzaSyBBkNkaBknWJBxodvip5n0YtPQUu2Se-b4')
+model = genai.GenerativeModel('gemini-pro')
+
+prompt = 'Give me a 3 line explanation of the concept of fundamental analysis for stocks.'
+print(model.generate_content(prompt).text)
